0324ab611bbf6cfd515b918015b59c0be228579a
Nikita <100973527+xaritoshaxnx@users.noreply.github.com>
2022-03-05 09:44:47 +0300

    Add files via upload

---
+++ src/main/java/com/example/vector/VectorApplication.java
@@ -0,0 +1,14 @@
+package com.example.vector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vector/VectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
